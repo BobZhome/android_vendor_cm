d90007d21c135ec2a9bbdd203abd2e49f055f58b
beerbong <yghazi@gmail.com>
2012-12-19 13:55:31 +0100

    UIModes and DPI groups (1/3)
    Change-Id: Iab2e72eaee69e6dd37b25cf6929dc54721096582

--- config/common.mk
+++ config/common.mk
@@ -119,6 +119,9 @@ PRODUCT_COPY_FILES += \
 # Don't export PS1 in /system/etc/mkshrc.
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/mkshrc:system/etc/mkshrc
+    
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/$(TARGET_PRODUCT).conf:system/etc/beerbong/properties.conf
 
 # T-Mobile theme engine
 include vendor/cm/config/themes_common.mk
