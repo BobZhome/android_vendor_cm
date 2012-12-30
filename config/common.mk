05ac1a3ddcd3b80c929aed9ce2e492410a591136
DvTonder <david.vantonder@gmail.com>
2012-12-30 13:37:40 -0500

    Vendor: Add the LockClock app (part 2 of 3)
    Change-Id: I152c9ddca2d5508819404d1c18c257f752da5749

--- config/common.mk
+++ config/common.mk
@@ -159,7 +159,8 @@ PRODUCT_PACKAGES += \
     DSPManager \
     libcyanogen-dsp \
     audio_effects.conf \
-    CMFileManager
+    CMFileManager \
+    LockClock
 
 # Extra tools in CM
 PRODUCT_PACKAGES += \
