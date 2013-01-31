dae6252c26ede82f02be849a1d5efcba57bef397
bobzhome <bobzhome@cheerful.com>
2013-01-30 19:15:34 -0500

    fix cherry-pick

--- vendorsetup.sh
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-for combo in $(cat vendor/cm/jenkins-build-targets)
-=======
-for combo in $(wget -o /dev/null -O - https://raw.github.com/CyanogenMod/hudson/master/cm-build-targets | grep cm-10.1 | awk {'print $1'})
->>>>>>> 13e5cb0... Update path for build roster
-do
-    add_lunch_combo $combo
-done
