73e3124b800aee61693d32777adfe10b3e42a89e
YamilGhazi <yghazi@gmail.com>
2013-02-12 11:57:12 +0100

    Added build script
    Change-Id: I547c4d9acd2d0d711e313b2d7e5b5de0ccdca705

---
+++ build.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# get current path
+reldir=`dirname $0`
+cd $reldir
+DIR=`pwd`
+
+# Colorize and add text parameters
+red=$(tput setaf 1)             #  red
+grn=$(tput setaf 2)             #  green
+cya=$(tput setaf 6)             #  cyan
+txtbld=$(tput bold)             # Bold
+bldred=${txtbld}$(tput setaf 1) #  red
+bldgrn=${txtbld}$(tput setaf 2) #  green
+bldblu=${txtbld}$(tput setaf 4) #  blue
+bldcya=${txtbld}$(tput setaf 6) #  cyan
+txtrst=$(tput sgr0)             # Reset
+
+THREADS="16"
+DEVICE="$1"
+SYNC="$2"
+
+# get current version
+VERSION=$(cat $DIR/vendor/cm/config/common.mk | grep 'JELLY_BEER_VERSION := *' | sed  's/JELLY_BEER_VERSION := //g')
+
+# get time of startup
+res1=$(date +%s.%N)
+
+# we don't allow scrollback buffer
+echo -e '\0033\0143'
+clear
+
+echo -e "${cya}Building ${bldcya}JellyBeer v$VERSION ${txtrst}";
+
+# sync with latest sources
+echo -e ""
+if [ "$SYNC" == "true" ]
+then
+   echo -e "${bldblu}Fetching latest sources ${txtrst}"
+   repo sync -j"$THREADS"
+   echo -e ""
+fi
+
+# setup environment
+echo -e "${bldblu}Setting up environment ${txtrst}"
+export USE_CCACHE=1
+. build/envsetup.sh
+
+# brunch device
+echo -e ""
+echo -e "${bldblu}Brunching device ${txtrst}"
+brunch $DEVICE;
+
+# finished? get elapsed time
+res2=$(date +%s.%N)
+echo "${bldgrn}Total time elapsed: ${txtrst}${grn}$(echo "($res2 - $res1) / 60"|bc ) minutes ($(echo "$res2 - $res1"|bc ) seconds) ${txtrst}"
