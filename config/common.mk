016d692cf2f66ae1519e687ebc0c9a483cfa31d6
YamilGhazi <yghazi@gmail.com>
2013-03-14 21:23:18 +0100

    Remove GooManager
    Change-Id: I691aa2d04650b58cc5274483f0a868ed2dc1d363

--- config/common.mk
+++ config/common.mk
@@ -103,8 +103,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES +=  \
     vendor/cm/proprietary/Term.apk:system/app/Term.apk \
-    vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so \
-    vendor/cm/prebuilt/common/apk/GooManager.apk:system/app/GooManager.apk
+    vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so
 
 # Bring in camera effects
 PRODUCT_COPY_FILES +=  \
